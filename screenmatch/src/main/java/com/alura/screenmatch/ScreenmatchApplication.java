84fca57f59bc2fd6bc93b04b79292756237891fc
Kevin Israel López <65631970+Kevin12-0@users.noreply.github.com>
2024-06-12 16:00:01 -0600

    Creaar clase, para consumir cualquier API/ consumir la API

--- screenmatch/src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.screenmatch.service.ConsumoAPI;
+
 /* para trabajar con spring es nesesario implemntar
  * 
  * CommandLineRunner
@@ -18,7 +20,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo con Spring");
+		/* hola mundo con spring */
+		/* System.out.println("Hola mundo con Spring"); */
+
+		/* variable para consumir la API */
+
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=it&apikey=14991e95");
+		System.out.println(json);
+
 	}
 
 }
